6f06c9c55b9c51559b81bbef011fe16235269c79
Garon Davis <garoncdavis@gmail.com>
2018-10-19 11:14:35 -0400

    set up app

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,19 +1,19 @@
 export class Config {
-  static taskCollectionName: string = "tasks";
-  static productsCollectionName = "products";
-  static offlineAccountsCollectionName: any = "accounts";
+  static taskCollectionName: string = 'tasks';
+  static productsCollectionName = 'products';
+  static offlineAccountsCollectionName: any = 'accounts';
   static logo: string =
-    "https://devcenter.kinvey.com/images/Progress_Kinvey_Primary.png";
-  static appLogo: string = "~/app/images/logo.png";
-  static appTitle: string = "ACME SDE";
-  static productsPageTitle: string = "Products";
-  static tasksPageTitle: string = "Tasks";
-  static filesPageTitle: string = "Files";
-  static mapPageTitle: string = "Map";
-  static arPageTitle: string = "Augment Reality";
-  static offlinePageTitle: string = "Offline";
-  static chatPageTitle: string = "Chat";
-  static appKey: any = "kid_ZJk02vOUFg";
-  static appSecret: any = "c1a32d5b3c474d0fa20a6f33fef396d5";
-  static accountsPageTitle: string = "Accounts";
+    'https://devcenter.kinvey.com/images/Progress_Kinvey_Primary.png';
+  static appLogo: string = '~/app/images/logo.png';
+  static appTitle: string = 'ACME SDE';
+  static productsPageTitle: string = 'Products';
+  static tasksPageTitle: string = 'Tasks';
+  static filesPageTitle: string = 'Files';
+  static mapPageTitle: string = 'Map';
+  static arPageTitle: string = 'Augment Reality';
+  static offlinePageTitle: string = 'Offline';
+  static chatPageTitle: string = 'Chat';
+  static appKey: any = 'kid_B1ik-nH-z';
+  static appSecret: any = 'b418aa4a7bf34294a56751fa19a9da10';
+  static accountsPageTitle: string = 'Accounts';
 }
