01ef90aca69806f8eb6c13c9762f1f75e7759545
Ignacio Fuentes <ignaciofuentes@outlook.com>
2019-05-23 00:45:54 -0400

    find doctor

--- src/app/patient/find-doctor/find-doctor.component.ts
+++ src/app/patient/find-doctor/find-doctor.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+class Doctor {
+  constructor(public id: number, public name: string, public image: string) {}
+}
 @Component({
-  selector: 'app-find-doctor',
-  templateUrl: './find-doctor.component.html',
-  styleUrls: ['./find-doctor.component.scss']
+  selector: "app-find-doctor",
+  templateUrl: "./find-doctor.component.html",
+  styleUrls: ["./find-doctor.component.scss"]
 })
 export class FindDoctorComponent implements OnInit {
+  selected = 0;
 
-  constructor() { }
+  images = [1, 2, 3];
+  tickets: any[];
+  counter: number;
+  doctors: any[];
+  constructor() {}
 
   ngOnInit() {
+    this.tickets = ["Measles", "Influenza", "upset", "big Concern"];
+    this.doctors = [];
+    this.counter = 0;
+    for (var i = 0; i < 8; i++) {
+      this.doctors.push(
+        new Doctor(
+          i,
+          "Dr. John Doe",
+          "https://2.gravatar.com/avatar/7fc56bb23fe5236b432b5b91597109a1?s=160"
+        )
+      );
+      this.counter = i;
+    }
+    this.counter = 0;
   }
-
+  markSelected(v) {
+    console.log(v);
+    this.selected = v;
+  }
 }
