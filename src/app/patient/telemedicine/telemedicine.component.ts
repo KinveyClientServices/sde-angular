07930402573f395621816746cce94e8981880ca6
Ignacio Fuentes <ignaciofuentes@outlook.com>
2019-05-23 00:02:09 -0400

    telemedicine

--- src/app/patient/telemedicine/telemedicine.component.ts
+++ src/app/patient/telemedicine/telemedicine.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+class Ticket {
+  constructor(public id: number, public name: string) {}
+}
 @Component({
-  selector: 'app-telemedicine',
-  templateUrl: './telemedicine.component.html',
-  styleUrls: ['./telemedicine.component.scss']
+  selector: "app-telemedicine",
+  templateUrl: "./telemedicine.component.html",
+  styleUrls: ["./telemedicine.component.scss"]
 })
 export class TelemedicineComponent implements OnInit {
+  selected = 0;
 
-  constructor() { }
+  images = [1, 2, 3];
+  tickets: any[];
+  counter: number;
+  constructor() {}
 
   ngOnInit() {
+    this.tickets = ["Measles", "Influenza", "upset", "big Concern"];
+    this.counter = 0;
   }
-
+  markSelected(v) {
+    console.log(v);
+    this.selected = v;
+  }
 }
