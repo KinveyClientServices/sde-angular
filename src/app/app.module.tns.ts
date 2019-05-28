50b264652d1abdd7eb82213a8fcbe8eb7029aaf6
Ignacio Fuentes <ignaciofuentes@outlook.com>
2019-05-28 13:15:14 -0400

    deleting unnecesary stuff

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -5,19 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { ProductsComponent } from "./products/products.component";
-import { FilesComponent } from "./files/files.component";
-import { SettingsComponent } from "./settings/settings.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { OfflineComponent } from "./offline/offline.component";
-import { MapComponent } from "./map/map.component";
-import { ChatComponent } from "./chat/chat.component";
-import { ArComponent } from "./ar/ar.component";
+
 import { LoginComponent } from "./login/login.component";
-import { AddTaskComponent } from "./tasks/add-task/add-task.component";
-import { FileDetailsComponent } from "./files/file-details/file-details.component";
+
 import { LayoutComponent } from "./layout/layout.component";
-import { AccountsComponent } from "./accounts/accounts.component";
-import { AccountDetailsComponent } from "./accounts/account-details/account-details.component";
 
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
 
@@ -31,7 +22,7 @@ import { NativeChatModule } from "@progress-nativechat/nativescript-nativechat/a
 import { isAndroid, isIOS, device, screen } from "tns-core-modules/platform";
 import { KinveyModule } from "kinvey-nativescript-sdk/angular";
 import { Config } from "./config";
-import { KendoGridEditFormComponent } from './kendo-grid-edit-form/kendo-grid-edit-form.component';
+import { KendoGridEditFormComponent } from "./kendo-grid-edit-form/kendo-grid-edit-form.component";
 
 declare var GMSServices: any;
 if (isIOS) {
@@ -42,19 +33,8 @@ if (isIOS) {
   declarations: [
     AppComponent,
     HomeComponent,
-    ArComponent,
-    ChatComponent,
-    AccountsComponent,
-    AccountDetailsComponent,
-    FilesComponent,
     LoginComponent,
-    MapComponent,
-    OfflineComponent,
     ProductsComponent,
-    SettingsComponent,
-    TasksComponent,
-    AddTaskComponent,
-    FileDetailsComponent,
     LayoutComponent,
     KendoGridEditFormComponent
   ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { ChatComponent } from "./chat/chat.component";
-import { FilesComponent } from "./files/files.component";
+
 import { LoginComponent } from "./login/login.component";
-import { MapComponent } from "./map/map.component";
-import { OfflineComponent } from "./offline/offline.component";
+
 import { ProductsComponent } from "./products/products.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { AddTaskComponent } from "./tasks/add-task/add-task.component";
-import { FileDetailsComponent } from "./files/file-details/file-details.component";
+
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LayoutComponent } from "./layout/layout.component";
-import { ArComponent } from "./ar/ar.component";
-import { AccountsComponent } from "./accounts/accounts.component";
-import { AccountDetailsComponent } from "./accounts/account-details/account-details.component";
-import { SettingsComponent } from "./settings/settings.component";
 
 import { KinveyModule } from "kinvey-angular-sdk";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
@@ -33,20 +25,9 @@ import { DialogsModule } from "@progress/kendo-angular-dialog";
   declarations: [
     AppComponent,
     HomeComponent,
-    ChatComponent,
-    FilesComponent,
     LoginComponent,
-    MapComponent,
-    OfflineComponent,
     ProductsComponent,
-    TasksComponent,
-    AddTaskComponent,
-    FileDetailsComponent,
     LayoutComponent,
-    ArComponent,
-    AccountsComponent,
-    AccountDetailsComponent,
-    SettingsComponent,
     KendoGridEditFormComponent
   ],
   imports: [
