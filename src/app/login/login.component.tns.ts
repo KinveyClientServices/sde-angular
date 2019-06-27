a87ce1657c9e9a81963c15aa8a48b151f44642f2
Garon Davis <garondavis@MCBOSGADAVIS2.local>
2019-06-27 14:38:44 -0400

    changed username

--- src/app/login/login.component.tns.ts
+++ src/app/login/login.component.tns.ts
@@ -11,8 +11,8 @@ import { RouterExtensions } from "nativescript-angular/router";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  username = "ignacio";
-  password = "ignacio";
+  username = "garon";
+  password = "1234";
   processing: boolean;
   logo: string;
   title: string;
